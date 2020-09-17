193c08604513f1688613eb0c99635074ef3e0419
Bart Frackiewicz <decomplosed@gmail.com>
2020-09-17 14:29:41 +0200

    wrapped subtitle inside Jumbotron.SubTitle

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ export default function App() {
       {jumboData.map((item) => (
         <Jumbotron key={item.id} direction={item.direction}>
           <Jumbotron.Title>{item.title}</Jumbotron.Title>
-          <p>{item.subTitle}</p>
+          <Jumbotron.SubTitle>{item.subTitle}</Jumbotron.SubTitle>
           <p>{item.image}</p>
           <p>{item.alt}</p>
         </Jumbotron>
