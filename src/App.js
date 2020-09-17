f5dd87a7dbeefc2aee61cd5369d058c2b8bde907
Bart Frackiewicz <decomplosed@gmail.com>
2020-09-17 14:22:21 +0200

    passed direction to Jumbotron

--- src/App.js
+++ src/App.js
@@ -3,7 +3,13 @@ import jumboData from './fixtures/jumbo.json';
 import Jumbotron from './components/jumbotron';
 
 export default function App() {
-  return <Jumbotron.Container>{jumboData.map(item => (
-    <Jumbotron key={item.id}></Jumbotron>
-  ))}</Jumbotron.Container>;
+  return (
+    <Jumbotron.Container>
+      {jumboData.map((item) => (
+        <Jumbotron key={item.id} direction={item.direction}>
+          <p>Hello</p>
+        </Jumbotron>
+      ))}
+    </Jumbotron.Container>
+  );
 }
