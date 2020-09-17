1dd5fb16dd63eba025484ce288ffe6c7b7ded8dd
Bart Frackiewicz <decomplosed@gmail.com>
2020-09-17 14:09:56 +0200

    spread rest of the props

--- src/components/jumbotron/index.js
+++ src/components/jumbotron/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Inner } from './styles/jumbotron';
 
-export default function Jumbotron({ direction = 'row' }) {
+export default function Jumbotron({ direction = 'row', ...restProps }) {
   return <Inner direction={direction}>I am the jumbotron!</Inner>;
 }
