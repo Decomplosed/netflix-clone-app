85c1d296b9b463e093aece051d22042c2d0c5ffd
Bart Frackiewicz <decomplosed@gmail.com>
2020-09-20 21:17:44 +0200

    added media query inside Title component

--- src/components/jumbotron/styles/jumbotron.js
+++ src/components/jumbotron/styles/jumbotron.js
@@ -27,6 +27,9 @@ export const Title = styled.h1`
   font-size: 50px;
   line-height: 1.1;
   margin-bottom: 8px;
+  @media (max-width: 600px) {
+    font-size: 35px;
+  }
 `;
 
 export const Container = styled.div``;
