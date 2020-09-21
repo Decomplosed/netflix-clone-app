c2b696e30de1cdd8c7d18b95d0470db5d477e0e0
Bart Frackiewicz <decomplosed@gmail.com>
2020-09-21 17:32:24 +0200

    imported in JumbotronContainer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React from 'react';
-import jumboData from './fixtures/jumbo.json';
-import Jumbotron from './components/jumbotron';
+import JumbotronContainer from './containers/jumbotron';
 
 export default function App() {
-  return (
-    <JumbotronContainer />
-  );
+  return <JumbotronContainer />;
 }
