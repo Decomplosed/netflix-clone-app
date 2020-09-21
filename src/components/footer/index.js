f7894c0637c847ecf33a7534c38d378a33988949
Bart Frackiewicz <decomplosed@gmail.com>
2020-09-21 17:43:49 +0200

    created FooterRow component

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -3,3 +3,7 @@ import React from 'react';
 export default function Footer({ children, ...restProps }) {
   return <Container {...restProps}>{children}</Container>;
 }
+
+Footer.Row = function FooterRow({ children, ...restProps }) {
+  return <Row {...restProps}>{children}</Row>;
+};
