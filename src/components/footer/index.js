633ca181ca0367125231e3b96cbeb0dfcbc93635
Bart Frackiewicz <decomplosed@gmail.com>
2020-09-22 23:50:40 +0200

    imported in styled components needed for Footer

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Container, Row, Column, Link, Title, Text } from './styles/footer';
 
 export default function Footer({ children, ...restProps }) {
   return <Container {...restProps}>{children}</Container>;
