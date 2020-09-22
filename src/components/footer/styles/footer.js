b374b54d4f66cbd2388ad659cc706802f27d5b42
Bart Frackiewicz <decomplosed@gmail.com>
2020-09-22 23:52:37 +0200

    added media query for Row

--- src/components/footer/styles/footer.js
+++ src/components/footer/styles/footer.js
@@ -21,4 +21,7 @@ export const Row = styled.div`
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(230px, 1fr));
   grid-gap: 15px;
+  @media (max-width: 1000px) {
+    grid-template-columns: repeat(auto-fill, minmax(150px, 1fr));
+  }
 `;
