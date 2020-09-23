97fcb985c5cca48d447ffaa6d6732f55733774ea
Bart Frackiewicz <decomplosed@gmail.com>
2020-09-23 23:51:24 +0200

    show children inside Inner

--- src/components/accordion/index.js
+++ src/components/accordion/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Accordion({ children, ...restProps }) {
   return (
     <Container {...restProps}>
-      <Inner></Inner>
+      <Inner>{children}</Inner>
     </Container>
   );
 }
