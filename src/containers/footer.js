df469c8f1d3148ccd9cd0383f3d228c9299e3e6d
Bart Frackiewicz <decomplosed@gmail.com>
2020-09-23 23:44:46 +0200

    added last bit of Links for Footer

--- src/containers/footer.js
+++ src/containers/footer.js
@@ -28,6 +28,13 @@ export function FooterContainer() {
           <Footer.Link href='#'>Privacy</Footer.Link>
           <Footer.Link href='#'>Speed Test</Footer.Link>
         </Footer.Column>
+
+        <Footer.Column>
+          <Footer.Link href='#'>Media Centre</Footer.Link>
+          <Footer.Link href='#'>Buy gift cards</Footer.Link>
+          <Footer.Link href='#'>Cookie Preferences</Footer.Link>
+          <Footer.Link href='#'>Legal Notices</Footer.Link>
+        </Footer.Column>
       </Footer.Row>
     </Footer>
   );
