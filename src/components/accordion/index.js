c4dc8fd8d5b91210d001ea071ab099b8b210a81a
Bart Frackiewicz <decomplosed@gmail.com>
2020-09-26 12:15:12 +0200

    if toggleShow is false show add icon

--- src/components/accordion/index.js
+++ src/components/accordion/index.js
@@ -41,7 +41,11 @@ Accordion.Header = function AccordionHeader({ children, ...restProps }) {
   return (
     <Header onClick={() => setToggleShow(!toggleShow)} {...restProps}>
       {children}
-      {toggleShow ? <img src='/images/icons/close-slim.png' alt='Close' />}
+      {toggleShow ? (
+        <img src='/images/icons/close-slim.png' alt='Close' />
+      ) : (
+        <img src='/images/icons/add.png' alt='Open' />
+      )}
     </Header>
   );
 };
