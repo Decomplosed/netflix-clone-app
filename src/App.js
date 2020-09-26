9ea29be35694cbac7ae716d2791e107642099540
Bart Frackiewicz <decomplosed@gmail.com>
2020-09-26 08:57:45 +0200

    imported in FaqsContainer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { JumbotronContainer } from './containers/jumbotron';
 import { FooterContainer } from './containers/footer';
+import { FaqsContainer } from './containers/footer';
 
 export default function App() {
   return (
