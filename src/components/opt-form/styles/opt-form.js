c411b7d6c4def8691e3e8033b978067030433a37
Bart Frackiewicz <decomplosed@gmail.com>
2020-09-27 09:00:31 +0200

    added styles for Container

--- src/components/opt-form/styles/opt-form.js
+++ src/components/opt-form/styles/opt-form.js
@@ -1,1 +1,9 @@
 import styled from 'styled-components/macro';
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  height: 100%;
+  margin-top: 20px;
+  flex-wrap: wrap;
+`;
