cd46d029fe69bfe45e9a87e34d390bbb9af68825
Bart Frackiewicz <decomplosed@gmail.com>
2020-09-27 09:01:34 +0200

    added styles for Button component

--- src/components/opt-form/styles/opt-form.js
+++ src/components/opt-form/styles/opt-form.js
@@ -25,3 +25,17 @@ export const Break = styled.div`
   flex-basis: 100%;
   height: 0;
 `;
+
+export const Button = styled.button`
+  display: flex;
+  align-items: center;
+  height: 100%;
+  height: 70px;
+  background: #e50914;
+  color: white;
+  text-transform: uppercase;
+  padding: 0 32px;
+  font-size: 26px;
+  border: 0;
+  cursor: pointer;
+`;
