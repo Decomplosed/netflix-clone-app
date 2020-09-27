58940a888f71333cf3be1860341c6005f8907ce7
Bart Frackiewicz <decomplosed@gmail.com>
2020-09-27 08:58:29 +0200

    passed ...restProps to Text

--- src/components/opt-form/styles/opt-form.js
+++ src/components/opt-form/styles/opt-form.js
@@ -17,5 +17,5 @@ OptForm.Button = function OptFormButton({ children, ...restProps }) {
 };
 
 OptForm.Text = function OptFormText({ children, ...restProps }) {
-  return <Text></Text>
+  return <Text {...restProps}></Text>;
 };
