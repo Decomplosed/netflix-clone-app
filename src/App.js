68d4812acf7ccf1c21fd871258959d15dfaed6e4
Bart Frackiewicz <decomplosed@gmail.com>
2020-09-28 23:50:10 +0200

    moved all necessary imports

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { JumbotronContainer } from './containers/jumbotron';
-import { FooterContainer } from './containers/footer';
-import { FaqsContainer } from './containers/faqs';
 import * as ROUTES from './constants/routes';
 
 export default function App() {
   return (
     <Router>
-      <Route exact path={ROUTES.HOME}>
-      </Route>
+      <Route exact path={ROUTES.HOME}></Route>
     </Router>
   );
 }
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { JumbotronContainer } from './containers/jumbotron';
+import { FooterContainer } from './containers/footer';
+import { FaqsContainer } from './containers/faqs';
 
 export default function Home() {
   return (
