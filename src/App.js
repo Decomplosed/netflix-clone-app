2ae475d8df78bc8735c442ae42a9197cec358e91
Bart Frackiewicz <decomplosed@gmail.com>
2020-09-28 23:51:30 +0200

    render Home under / path

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import * as ROUTES from './constants/routes';
+import { Home } from './pages/home';
 
 export default function App() {
   return (
     <Router>
-      <Route exact path={ROUTES.HOME}></Route>
+      <Route exact path={ROUTES.HOME}>
+        <Home />
+      </Route>
     </Router>
   );
 }
