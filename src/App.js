7a244bf3656c3c22617d10d56baea3d69670ab56
Bart Frackiewicz <decomplosed@gmail.com>
2020-09-28 23:46:28 +0200

    imported in Route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { JumbotronContainer } from './containers/jumbotron';
 import { FooterContainer } from './containers/footer';
 import { FaqsContainer } from './containers/faqs';
