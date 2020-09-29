b562ff90c65e13b135852299c4b4a9d55ed98d9d
Bart Frackiewicz <decomplosed@gmail.com>
2020-09-29 21:16:40 +0200

    created body for HeaderLogo component

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -8,4 +8,10 @@ Header.Frame = function HeaderFrame({ children, ...restProps }) {
   return <Container {...restProps}>{children}</Container>;
 };
 
-Header.Logo = function HeaderLogo({ to, ...restProps }) {};
+Header.Logo = function HeaderLogo({ to, ...restProps }) {
+  return (
+    <ReactRouterLink to={to}>
+      <Logo {...restProps} />
+    </ReactRouterLink>
+  );
+};
