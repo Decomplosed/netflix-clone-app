af40b3e09b1c18e810460a1a1fff71ceb32a5efe
Bart Frackiewicz <decomplosed@gmail.com>
2020-09-29 21:14:49 +0200

    created body of HeaderFrame

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -4,4 +4,6 @@ export default function Header({ bg = true, children, ...restProps }) {
   return bg ? <Background {...restProps}>{children}</Background> : children;
 }
 
-Header.Frame = function HeaderFrame({ children, ...restProps }) {};
+Header.Frame = function HeaderFrame({ children, ...restProps }) {
+  return <Container {...restProps}>{children}</Container>;
+};
