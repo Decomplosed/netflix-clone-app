00c50adb1cc9f03a587858bbcddfae6f45e74042
Bart Frackiewicz <decomplosed@gmail.com>
2020-09-29 21:14:22 +0200

    declared HeaderFrame component

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -3,3 +3,5 @@ import React from 'react';
 export default function Header({ bg = true, children, ...restProps }) {
   return bg ? <Background {...restProps}>{children}</Background> : children;
 }
+
+Header.Frame = function HeaderFrame({ children, ...restProps }) {};
