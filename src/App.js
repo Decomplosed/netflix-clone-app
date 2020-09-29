4b5ec1c60e79e49c50c8d01e658dd56461c6180f
Bart Frackiewicz <decomplosed@gmail.com>
2020-09-29 20:58:38 +0200

    setup route for sign up page

--- src/App.js
+++ src/App.js
@@ -15,6 +15,9 @@ export default function App() {
       <Route exact path={ROUTES.SIGN_IN}>
         <Signin />
       </Route>
+      <Route exact path={ROUTES.SIGN_UP}>
+        <Signup />
+      </Route>
     </Router>
   );
 }
