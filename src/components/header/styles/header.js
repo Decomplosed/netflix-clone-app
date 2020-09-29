e77e63a415787d01d3975b4d771a362d44f57efb
Bart Frackiewicz <decomplosed@gmail.com>
2020-09-29 21:37:56 +0200

    created styled Text component

--- src/components/header/styles/header.js
+++ src/components/header/styles/header.js
@@ -46,3 +46,10 @@ export const Group = styled.div`
   display: flex;
   align-items: center;
 `;
+
+export const Text = styled.p`
+  color: white;
+  font-size: 22px;
+  line-height: normal;
+  text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.45);
+`;
