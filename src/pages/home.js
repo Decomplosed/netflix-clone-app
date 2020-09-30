e768ae758dbd34349f6ca414aa89f3145eddf3dc
Bart Frackiewicz <decomplosed@gmail.com>
2020-09-30 23:29:10 +0200

    wrapped all components inside HeaderCotnainer at  home page

--- src/pages/home.js
+++ src/pages/home.js
@@ -7,9 +7,11 @@ import { FaqsContainer } from '../containers/faqs';
 export default function Home() {
   return (
     <>
-      <JumbotronContainer />
-      <FaqsContainer />
-      <FooterContainer />
+      <HeaderContainer>
+        <JumbotronContainer />
+        <FaqsContainer />
+        <FooterContainer />
+      </HeaderContainer>
     </>
   );
 }
