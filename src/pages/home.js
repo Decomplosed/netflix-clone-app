889be3ced8fee4be288299024904ca8ddd5766bc
Bart Frackiewicz <decomplosed@gmail.com>
2020-09-30 23:51:35 +0200

    render OptForm inside HeaderContainer

--- src/pages/home.js
+++ src/pages/home.js
@@ -8,7 +8,17 @@ import { FaqsContainer } from '../containers/faqs';
 export default function Home() {
   return (
     <>
-      <HeaderContainer></HeaderContainer>
+      <HeaderContainer>
+        <OptForm>
+          <OptForm.Input placeholder='Email address' />
+          <OptForm.Button>Try it now</OptForm.Button>
+          <OptForm.Break />
+          <OptForm.Text>
+            Ready to watch? Enter your email to create or restart your
+            membership.
+          </OptForm.Text>
+        </OptForm>
+      </HeaderContainer>
       <JumbotronContainer />
       <FaqsContainer />
       <FooterContainer />
