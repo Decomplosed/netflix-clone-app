3ec9a4089f5bc220e796d12eea22a5d7e441df01
Bart Frackiewicz <decomplosed@gmail.com>
2020-09-30 23:43:29 +0200

    imported in styled ButtonLink

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link as ReactRouterLink } from 'react-router-dom';
-import { Container, Logo, Background } from './styles/header';
+import { Container, Logo, Background, ButtonLink } from './styles/header';
 
 export default function Header({ bg = true, children, ...restProps }) {
   return bg ? <Background {...restProps}>{children}</Background> : children;
