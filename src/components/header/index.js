40f95705d946d73fa7fa57fe12d57887509abf3c
Bart Frackiewicz <decomplosed@gmail.com>
2020-09-30 23:38:49 +0200

    created ButtonLink component

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -10,6 +10,10 @@ Header.Frame = function HeaderFrame({ children, ...restProps }) {
   return <Container {...restProps}>{children}</Container>;
 };
 
+Header.ButtonLink = function ButtonLink({ children, ...restProps }) {
+  return <ButtonLink {...restProps}>{children}</ButtonLink>;
+};
+
 Header.Logo = function HeaderLogo({ to, ...restProps }) {
   return (
     <ReactRouterLink to={to}>
