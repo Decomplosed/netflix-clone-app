9141a313eb77339d2b22c0d4d9e1f2e3478393fe
Bart Frackiewicz <decomplosed@gmail.com>
2020-09-30 23:52:31 +0200

    added Feature.Title

--- src/pages/home.js
+++ src/pages/home.js
@@ -10,7 +10,7 @@ export default function Home() {
     <>
       <HeaderContainer>
         <Feature>
-          
+          <Feature.Title></Feature.Title>
         </Feature>
         <OptForm>
           <OptForm.Input placeholder='Email address' />
