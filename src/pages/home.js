72747ceebb675577776782a58e85b085066e9833
Bart Frackiewicz <decomplosed@gmail.com>
2020-09-30 23:28:44 +0200

    imported in HeaderContainer

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { HeaderContainer } from '../containers/header';
 import { JumbotronContainer } from '../containers/jumbotron';
 import { FooterContainer } from '../containers/footer';
 import { FaqsContainer } from '../containers/faqs';
