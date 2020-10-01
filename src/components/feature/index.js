8eab43dcc3dec22e4d36a2fde5a28d5913cb7734
Bart Frackiewicz <decomplosed@gmail.com>
2020-10-01 22:47:27 +0200

    return Container

--- src/components/feature/index.js
+++ src/components/feature/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
 import { Container, Title, SubTitle } from './styles/feature';
 
-export default function Feature({ children, ...restProps }) {}
+export default function Feature({ children, ...restProps }) {
+  return <Container></Container>
+}
