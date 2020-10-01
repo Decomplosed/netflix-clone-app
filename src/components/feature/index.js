a664056553bae1cafb3b64156e15db097523232d
Bart Frackiewicz <decomplosed@gmail.com>
2020-10-01 22:48:15 +0200

    crae

--- src/components/feature/index.js
+++ src/components/feature/index.js
@@ -4,3 +4,5 @@ import { Container, Title, SubTitle } from './styles/feature';
 export default function Feature({ children, ...restProps }) {
   return <Container {...restProps}>{children}</Container>;
 }
+
+Feature.Title = function FeatureTitle() {}
