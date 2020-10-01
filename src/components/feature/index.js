7e913c346b7d36dc170044e1f229bd08239e4961
Bart Frackiewicz <decomplosed@gmail.com>
2020-10-01 22:47:40 +0200

    passed restProps to the Container

--- src/components/feature/index.js
+++ src/components/feature/index.js
@@ -2,5 +2,5 @@ import React from 'react';
 import { Container, Title, SubTitle } from './styles/feature';
 
 export default function Feature({ children, ...restProps }) {
-  return <Container></Container>
+  return <Container {...restProps}></Container>;
 }
