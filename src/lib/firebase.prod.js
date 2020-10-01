b5f032f7f21ce9b7fd47ca10aed9cf6db88b474f
Bart Frackiewicz <decomplosed@gmail.com>
2020-10-01 23:27:23 +0200

    setup the config object with environmental variables

--- src/lib/firebase.prod.js
+++ src/lib/firebase.prod.js
@@ -2,7 +2,15 @@ import Firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 
-const config = {};
+const config = {
+  apiKey: process.env.REACT_APP_FIREABASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREABASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREABASE_DATABASE_UR,
+  projectId: process.env.REACT_APP_FIREABASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREABASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREABASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREABASE_APP_ID,
+};
 
 const firebase = Firebase.initializeApp(config);
 
