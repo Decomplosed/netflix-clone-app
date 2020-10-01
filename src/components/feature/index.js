45894c182af973da9dc375a34996339e815447aa
Bart Frackiewicz <decomplosed@gmail.com>
2020-10-01 22:48:49 +0200

    extracted children and restoProps

--- src/components/feature/index.js
+++ src/components/feature/index.js
@@ -5,4 +5,4 @@ export default function Feature({ children, ...restProps }) {
   return <Container {...restProps}>{children}</Container>;
 }
 
-Feature.Title = function FeatureTitle() {}
+Feature.Title = function FeatureTitle({ children, ...restProps }) {};
