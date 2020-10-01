d0c8e8c11b0504c9cafb914b6a0d6cd6297fd472
Bart Frackiewicz <decomplosed@gmail.com>
2020-10-01 22:47:54 +0200

    render children within Container

--- src/components/feature/index.js
+++ src/components/feature/index.js
@@ -2,5 +2,5 @@ import React from 'react';
 import { Container, Title, SubTitle } from './styles/feature';
 
 export default function Feature({ children, ...restProps }) {
-  return <Container {...restProps}></Container>;
+  return <Container {...restProps}>{children}</Container>;
 }
