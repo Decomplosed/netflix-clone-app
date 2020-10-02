608438b56d65a48eb9bea3493d765ad712204756
Bart Frackiewicz <decomplosed@gmail.com>
2020-10-02 14:55:31 +0200

    passed all styled components that will be needed for form

--- src/components/form/index.js
+++ src/components/form/index.js
@@ -1,5 +1,14 @@
 import React from 'react';
-import { Container } from './styles/form';
+import {
+  Container,
+  Error,
+  Title,
+  Text,
+  TextSmall,
+  Link,
+  Input,
+  Submit,
+} from './styles/form';
 
 export default function Form({ children, ...restProps }) {
   return <Container {...restProps}>{children}</Container>;
