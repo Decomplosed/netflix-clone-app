23ac1eab2bde1a097848a04e54cc2bfe2c6bccd4
Bart Frackiewicz <decomplosed@gmail.com>
2020-10-02 14:59:37 +0200

    created FormSubmit component

--- src/components/form/index.js
+++ src/components/form/index.js
@@ -42,3 +42,7 @@ Form.Link = function FormLink({ children, ...restProps }) {
 Form.Input = function FormInput({ children, ...restProps }) {
   return <Input {...restProps}>{children}</Input>;
 };
+
+Form.Submit = function FormSubmit({ children, ...restProps }) {
+  return <Submit {...restProps}>{children}</Submit>;
+};
