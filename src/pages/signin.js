0880e1a8dad9a38b5d14f9b46f2edaaa615a2654
Bart Frackiewicz <decomplosed@gmail.com>
2020-10-02 14:50:16 +0200

    imported in FooterContainer

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { HeaderContainer } from '../containers/header';
+import { FooterContainer } from '../containers/footer';
 
 export default function Signin() {
   return <HeaderContainer></HeaderContainer>;
