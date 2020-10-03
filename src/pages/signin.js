3175c416eb4eb9ce11f2c60b334086a239aad257
Bart Frackiewicz <decomplosed@gmail.com>
2020-10-03 21:11:07 +0200

    imported in Form

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { HeaderContainer } from '../containers/header';
 import { FooterContainer } from '../containers/footer';
+import { Form } from '../components/';
 
 export default function Signin() {
   return (
