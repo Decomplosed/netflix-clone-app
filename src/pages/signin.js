d6615b96b51f344e3a64861d1820cba10766a124
Bart Frackiewicz <decomplosed@gmail.com>
2020-10-03 21:21:00 +0200

    passed autoComplete as off for the password input

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -23,6 +23,7 @@ export default function Signin() {
             <Form.Input
               type='password'
               placeholder='Password'
+              autoComplete='off'
               value={password}
               onChange={({ target }) => setPassword(target.value)}
             />
