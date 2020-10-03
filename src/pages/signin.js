cda0208a6dd03c07b32f8e138dc884e9a16726ba
Bart Frackiewicz <decomplosed@gmail.com>
2020-10-03 21:11:21 +0200

    imported useState

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { HeaderContainer } from '../containers/header';
 import { FooterContainer } from '../containers/footer';
 import { Form } from '../components/';
