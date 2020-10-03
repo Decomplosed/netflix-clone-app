ec8351b7fadc5e87316604fbda2b89a6fa4b26e8
Bart Frackiewicz <decomplosed@gmail.com>
2020-10-03 21:13:43 +0200

    changed initial value of password to empty string

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -5,7 +5,7 @@ import { Form } from '../components/';
 
 export default function Signin() {
   const [emailAddress, setEmailAddress] = useState('');
-  const [password, setPassword] = useState();
+  const [password, setPassword] = useState('');
   const [error, setError] = useState('');
 
   return (
