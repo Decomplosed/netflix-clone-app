b62f83a5f52744abe9ba12b54c260e23c92afc5b
Bart Frackiewicz <decomplosed@gmail.com>
2020-10-04 21:41:21 +0200

    show Form.Error if error is a thing

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -25,6 +25,7 @@ export default function Signup() {
       <HeaderContainer>
         <Form>
           <Form.Title>Sign Up</Form.Title>
+          {error && <Form.Error>{error}</Form.Error>}
         </Form>
       </HeaderContainer>
     </>
