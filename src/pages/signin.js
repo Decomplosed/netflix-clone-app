8829a55ff660de09757c1577b06256d0307f4874
Bart Frackiewicz <decomplosed@gmail.com>
2020-10-04 21:25:05 +0200

    setEmailAddress to empty string on error

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -19,6 +19,7 @@ export default function Signin() {
       .signInWithEmailAndPassword(emailAddress, address)
       .then(() => {})
       .catch((error) => {
+        setEmailAddress('');
       });
   };
 
