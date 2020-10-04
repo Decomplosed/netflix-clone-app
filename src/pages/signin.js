ce0ed80b5d647cea563e57d1904008f1c6f1c232
Bart Frackiewicz <decomplosed@gmail.com>
2020-10-04 21:30:26 +0200

    imported useHistory

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,4 +1,5 @@
 import React, { useState, useContext } from 'react';
+import { useHistory } from 'react-router-dom';
 import { FirebaseContext } from '../context/firebase';
 import { HeaderContainer } from '../containers/header';
 import { FooterContainer } from '../containers/footer';
