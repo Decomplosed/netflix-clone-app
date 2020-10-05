69f8395cf6d73f11b5e0f61491a1b6095a0b8189
Bart Frackiewicz <decomplosed@gmail.com>
2020-10-05 21:27:01 +0200

    return Route

--- src/helpers/routes.js
+++ src/helpers/routes.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 
-export function IsUserRedirect({
-  user,
-  loggedInPath,
-  children,
-  ...restProps
-}) {}
+export function IsUserRedirect({ user, loggedInPath, children, ...rest }) {
+  return <Route {...rest} />;
+}
