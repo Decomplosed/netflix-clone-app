f5e57e114d5d019fa296a2a492c5481718223908
Bart Frackiewicz <decomplosed@gmail.com>
2020-10-05 21:32:09 +0200

    declared user as an empty object for now

--- src/App.js
+++ src/App.js
@@ -2,22 +2,24 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import * as ROUTES from './constants/routes';
 import { Home, Browse, Signin, Signup } from './pages';
-import { isUserRedirect } from './helpers/routes';
+import { IsUserRedirect, isUserRedirect } from './helpers/routes';
 
 export default function App() {
+  const user = {};
+
   return (
     <Router>
-      <Route exact path={ROUTES.HOME}>
-        <Home />
-      </Route>
-      <Route exact path={ROUTES.BROWSE}>
-        <Browse />
-      </Route>
       <Route exact path={ROUTES.SIGN_IN}>
         <Signin />
       </Route>
       <Route exact path={ROUTES.SIGN_UP}>
         <Signup />
+      </Route>
+      <Route exact path={ROUTES.HOME}>
+        <Home />
+      </Route>
+      <Route exact path={ROUTES.BROWSE}>
+        <Browse />
       </Route>
     </Router>
   );
