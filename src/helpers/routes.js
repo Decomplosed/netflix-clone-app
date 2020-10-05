cbd95bed89c028150339c2b84e397528a8d64884
Bart Frackiewicz <decomplosed@gmail.com>
2020-10-05 21:28:10 +0200

    if there's not a user return children

--- src/helpers/routes.js
+++ src/helpers/routes.js
@@ -2,5 +2,12 @@ import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 
 export function IsUserRedirect({ user, loggedInPath, children, ...rest }) {
-  return <Route {...rest} render={() => {}} />;
+  return (
+    <Route
+      {...rest}
+      render={() => {
+        if (!user) return children;
+      }}
+    />
+  );
 }
