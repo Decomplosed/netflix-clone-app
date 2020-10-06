a40c9d4b220e8d823a9b5f49bcd873f258d82f36
Bart Frackiewicz <decomplosed@gmail.com>
2020-10-06 13:33:17 +0200

    created a useState case for user value inside the state and check whether localStorage already contains user data

--- src/hooks/use-auth-listener.js
+++ src/hooks/use-auth-listener.js
@@ -1,4 +1,8 @@
 import React, { useState, useEffect, useContext } from 'react';
 import { FirebaseContext } from '../context/firebase';
 
-export default function useAuthListener() {}
+export default function useAuthListener() {
+  const [user, setUser] = useState(
+    JSON.parse(localStorage.getItem('authUser')),
+  );
+}
