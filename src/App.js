3abf38e9c08fe75574f234c06a6d6310ac21c52f
Bart Frackiewicz <decomplosed@gmail.com>
2020-10-06 13:24:47 +0200

    remved exact and set user to null instead of an empty object

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { Home, Browse, Signin, Signup } from './pages';
 import { IsUserRedirect, ProtectedRoute } from './helpers/routes';
 
 export default function App() {
-  const user = {};
+  const user = null;
 
   return (
     <Router>
@@ -13,7 +13,6 @@ export default function App() {
         user={user}
         loggedInPath={ROUTES.BROWSE}
         path={ROUTES.SIGN_IN}
-        exact
       >
         <Signin />
       </IsUserRedirect>
@@ -21,7 +20,6 @@ export default function App() {
         user={user}
         loggedInPath={ROUTES.BROWSE}
         path={ROUTES.SIGN_UP}
-        exact
       >
         <Signup />
       </IsUserRedirect>
