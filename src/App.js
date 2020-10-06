0e34efb85688e37ed7b9fb8e61fb9269c8d22bca
Bart Frackiewicz <decomplosed@gmail.com>
2020-10-06 10:40:03 +0200

    imported in ProtectedRoute

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import * as ROUTES from './constants/routes';
 import { Home, Browse, Signin, Signup } from './pages';
-import { IsUserRedirect } from './helpers/routes';
+import { IsUserRedirect, ProtectedRoute } from './helpers/routes';
 
 export default function App() {
   const user = {};
