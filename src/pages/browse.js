7d6702fcd44a0bf952f4848e85aed7bee18971b2
Bart Frackiewicz <decomplosed@gmail.com>
2020-10-07 15:02:05 +0200

    imported selectionFilter

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useContent } from '../hooks';
+import selectionFilter from '../utils/selection-filter';
 
 export default function Browse() {
   const { series } = useContent('series');
