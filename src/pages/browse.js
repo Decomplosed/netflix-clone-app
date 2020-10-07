43f70bc9095916f8aca78028687d549ae3b17cfe
Bart Frackiewicz <decomplosed@gmail.com>
2020-10-07 15:03:22 +0200

    keep the filtered data under slides variable

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -5,6 +5,7 @@ import selectionFilter from '../utils/selection-filter';
 export default function Browse() {
   const { series } = useContent('series');
   const { films } = useContent('films');
+  const slides = selectionFilter({ series, films });
 
   return <p>BROWSE</p>;
 }
