b5504fe3a1fd1545653e58590f08a1b77dce57ee
Bart Frackiewicz <decomplosed@gmail.com>
2020-10-07 14:38:22 +0200

    added useState case for content

--- src/hooks/use-content.js
+++ src/hooks/use-content.js
@@ -1,4 +1,6 @@
 import { useEffect, useState, useContext } from 'react';
 import { FirebaseContext } from '../context/firebase';
 
-export default function useContent(target) {}
+export default function useContent(target) {
+  const [content, setContent] = useState([]);
+}
