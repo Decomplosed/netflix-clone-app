b8a27ec36eedc7c228627883a5736e3f4bc95e91
Bart Frackiewicz <decomplosed@gmail.com>
2020-10-07 14:55:19 +0200

    return documentaries type series

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -2,5 +2,8 @@ import React from 'react';
 import { useContent } from '../hooks';
 
 export default function Browse() {
+  const { series } = useContent('series');
+  const { films } = useContent('films');
+
   return <p>BROWSE</p>;
 }
--- src/utils/selection-map.js
+++ src/utils/selection-map.js
@@ -1,1 +1,10 @@
-export default function selectionMap({ series, films }) {}
+export default function selectionMap({ series, films }) {
+  return {
+    series: [
+      {
+        title: 'Documentaries',
+        data: series.filter((item) => item.genre === 'documentaries'),
+      },
+    ],
+  };
+}
