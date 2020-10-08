b999f0527de35c0545e39eedebe263bd2e17a0ca
Bart Frackiewicz <decomplosed@gmail.com>
2020-10-08 08:31:53 +0200

    return dummy information about the component for now

--- src/containers/profiles.js
+++ src/containers/profiles.js
@@ -1,3 +1,5 @@
 import React from 'react';
 
-export function SelectProfileContainer({user, setProfile}) {}
+export function SelectProfileContainer({ user, setProfile }) {
+  return <p>SelectionProfile</p>;
+}
