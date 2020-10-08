a3467a1edfa598e659316f9a258b91e4ca164a7f
Bart Frackiewicz <decomplosed@gmail.com>
2020-10-08 08:41:48 +0200

    created ProfilesTitle

--- src/components/profiles/styles/profiles.js
+++ src/components/profiles/styles/profiles.js
@@ -4,3 +4,7 @@ import { Container, Title, List, Item, Picture, Name } from './styles/profiles';
 export default function Profiles({ children, ...restProps }) {
   return <Container {...restProps}>{children}</Container>;
 }
+
+Profiles.Title = function ProfilesTitle({ children, ...restProps }) {
+  return <Title {...restProps}>{children}</Title>;
+};
