1cee06f6e1eb19c8117b225727a7be7286e3d1fa
Bart Frackiewicz <decomplosed@gmail.com>
2020-10-08 08:33:05 +0200

    imported in logo

--- src/containers/profiles.js
+++ src/containers/profiles.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import * as ROUTES from '../constants/routes';
+import logo from '../logo.svg';
 
 export function SelectProfileContainer({ user, setProfile }) {
   return <p>SelectionProfile</p>;
