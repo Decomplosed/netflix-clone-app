69320e1ace9e0eb1407e010695e4c578b78fbf08
Bart Frackiewicz <decomplosed@gmail.com>
2020-10-08 08:44:25 +0200

    styled the Container

--- src/components/profiles/styles/profiles.js
+++ src/components/profiles/styles/profiles.js
@@ -1,1 +1,10 @@
 import styled from 'styled-components/macro';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: auto;
+  max-width: 80%;
+`;
