aa15610dc39ebe57d69984ceb6b83a8263d7538d
Bart Frackiewicz <decomplosed@gmail.com>
2020-10-09 08:15:24 +0200

    styled the Picture component

--- src/components/loading/styles/loading.js
+++ src/components/loading/styles/loading.js
@@ -11,3 +11,13 @@ export const ReleaseBody = createGlobalStyle`
     overflow: visible;
   }
 `;
+
+export const Picture = styled.img`
+  width: 50px;
+  height: 50px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  margin-top: -100px;
+  margin-left: -22px;
+`;
