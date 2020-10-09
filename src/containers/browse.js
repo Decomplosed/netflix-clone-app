52c5c78849b25c2f60f17402a744a8af90c8ffd8
Bart Frackiewicz <decomplosed@gmail.com>
2020-10-09 07:49:54 +0200

    imported in useContext hook

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { SelectProfileContainer } from './profiles';
 import { FirebaseContext } from '../context/firebase';
 
