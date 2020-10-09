385503f2cc9c04dfe42667fa72da75f15b110226
Bart Frackiewicz <decomplosed@gmail.com>
2020-10-09 07:48:12 +0200

    passed user to SelectProfileContainer

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -2,5 +2,5 @@ import React from 'react';
 import { SelectProfileContainer } from './profiles';
 
 export function BrowseContainer({ slides }) {
-  return <SelectProfileContainer />;
+  return <SelectProfileContainer user={user} />;
 }
