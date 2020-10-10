76cc7bf09011a3fcf99978c1ddeba97ff6e9cf5e
Bart Frackiewicz <decomplosed@gmail.com>
2020-10-10 21:26:25 +0200

    extracted children, ...restProps

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -6,7 +6,7 @@ export default function Header({ bg = true, children, ...restProps }) {
   return bg ? <Background {...restProps}>{children}</Background> : children;
 }
 
-Header.Feature = function HeaderFeature() {};
+Header.Feature = function HeaderFeature({ children, ...restProps }) {};
 
 Header.Frame = function HeaderFrame({ children, ...restProps }) {
   return <Container {...restProps}>{children}</Container>;
