1a2ababd715cee29bad46e13ba9ac0093408bd23
Bart Frackiewicz <decomplosed@gmail.com>
2020-10-10 21:12:36 +0200

    styled pseudo elemen after of Spinner

--- src/components/loading/styles/loading.js
+++ src/components/loading/styles/loading.js
@@ -19,6 +19,23 @@ export const Spinner = styled.div`
   background-color: black;
   z-index: 999;
 
+  :after {
+    content: '';
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    background-image: url(/images/misc/spinner.png);
+    background-size: contain;
+    background-repeat: no-repeat;
+    margin-top: -150px;
+    margin-left: -75px;
+    width: 150px;
+    height: 150px;
+    animation-name: spin;
+    animation-duration: 1000ms;
+    animation-iteration-count: infinite;
+    animation-timing-function: linear;
+
   @-ms-keyframes spin {
     from {
       -ms-transform: rotate(0deg);
