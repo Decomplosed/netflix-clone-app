866028736e747c3c272d69927c89ce7e38bdf75b
Bart Frackiewicz <decomplosed@gmail.com>
2020-10-11 08:57:32 +0200

    changed Header.Picture to be self closing

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -29,7 +29,7 @@ export function BrowseContainer({ slides }) {
             <Header.TextLink>Films</Header.TextLink>
           </Header.Group>
           <Header.Group>
-            <Header.Picture src={user.photoURL}></Header.Picture>
+            <Header.Picture src={user.photoURL} />
           </Header.Group>
         </Header.Frame>
         <Header.Feature>
