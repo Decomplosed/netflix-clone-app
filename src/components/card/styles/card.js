2a3e2a93b1c5347c2bb26ee8dbc24b1bce71134a
Bart Frackiewicz <decomplosed@gmail.com>
2020-10-13 08:25:04 +0200

    added styled COntainer

--- src/components/card/styles/card.js
+++ src/components/card/styles/card.js
@@ -1,1 +1,8 @@
 import styled from 'styled-components/macro';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 50px;
+  box-sizing: border-box;
+`;
