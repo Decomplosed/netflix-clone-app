c49357a71b2f1f92a109df17250913bd640b540f
Bart Frackiewicz <decomplosed@gmail.com>
2020-10-13 08:27:11 +0200

    created styled Text component

--- src/components/card/styles/card.js
+++ src/components/card/styles/card.js
@@ -48,3 +48,13 @@ export const SubTitle = styled.p`
   user-select: none;
   display: none;
 `;
+
+export const Text = styled.p`
+  margin-top: 5px;
+  font-size: 10px;
+  color: #fff;
+  margin-bottom: 0;
+  user-select: none;
+  display: none;
+  line-height: normal;
+`;
