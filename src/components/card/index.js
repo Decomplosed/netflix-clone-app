7e24d2b1c0ac8a15d8da259426da9d289cd6726e
Bart Frackiewicz <decomplosed@gmail.com>
2020-10-13 08:37:34 +0200

    created useState case for showFeature value

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -2,4 +2,6 @@ import React, { useState, useContext, createContext } from 'react';
 
 export const FeatureContext = createContext();
 
-export default function Card({ children, ...restProps }) {}
+export default function Card({ children, ...restProps }) {
+  const [showFeature, setShowFeature] = useState(false);
+}
