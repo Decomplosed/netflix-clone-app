820534e65ba0e6a03c867d14b01a564a0f49dd99
Bart Frackiewicz <decomplosed@gmail.com>
2020-10-14 09:02:34 +0200

    return FeatureContext.Provider

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -5,4 +5,8 @@ export const FeatureContext = createContext();
 export default function Card({ children, ...restProps }) {
   const [showFeature, setShowFeature] = useState(false);
   const [itemFeature, setItemFeature] = useState({});
+
+  return (
+    <FeatureContext.Provider value={}></FeatureContext.Provider>
+  )
 }
