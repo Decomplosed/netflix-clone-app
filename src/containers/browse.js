c14dae82dc3ad3e2a477dabb49fe0dc0e23efe71
Bart Frackiewicz <decomplosed@gmail.com>
2020-10-16 00:08:25 +0200

    commented out Card.Feature for now

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -97,7 +97,7 @@ export function BrowseContainer({ slides }) {
                 </Card.Item>
               ))}
             </Card.Entities>
-            <Card.Feature category={category}></Card.Feature>
+            {/* <Card.Feature category={category}></Card.Feature> */}
           </Card>
         ))}
       </Card.Group>
