b750f443f9a0f35ec9737d37d85f537d2781d6d6
Bart Frackiewicz <decomplosed@gmail.com>
2020-10-16 10:46:48 +0200

    wrapped Player inside Card.Feature

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -97,11 +97,12 @@ export function BrowseContainer({ slides }) {
                 </Card.Item>
               ))}
             </Card.Entities>
-            <Card.Feature category={category}></Card.Feature>
-            <Player>
-              <Player.Button />
-              <Player.Video src='/videos/bunny.mp4' />
-            </Player>
+            <Card.Feature category={category}>
+              <Player>
+                <Player.Button />
+                <Player.Video src='/videos/bunny.mp4' />
+              </Player>
+            </Card.Feature>
           </Card>
         ))}
       </Card.Group>
