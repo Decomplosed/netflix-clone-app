5a4378b395e09a19e24a675b598405a00213c90e
Bart Frackiewicz <decomplosed@gmail.com>
2020-10-16 10:49:19 +0200

    show feature if showFeature is true

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -80,4 +80,8 @@ Card.Feature = function CardFeature({ children, category, ...restProps }) {
   const { showFeature, itemFeature, setShowFeature } = useContext(
     FeatureContext,
   );
+
+  return showFeature ? (
+    <Feature src={`/images/${category}/${itemFeature.genre}/${itemFeature.slug}/large.jpg`}
+  )
 };
