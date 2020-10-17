267cdac7e4e0df738f77ee923f25254522673252
Bart Frackiewicz <decomplosed@gmail.com>
2020-10-17 08:27:30 +0200

    styled Overlay component

--- src/components/player/styles/player.js
+++ src/components/player/styles/player.js
@@ -1,3 +1,16 @@
 import styled from 'styled-components/macro';
 
 export const Container = styled.div``;
+
+export const Overlay = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  margin: 0 20px;
+`;
