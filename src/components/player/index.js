649d9a49921876ff00b4a6edee81239be878d8a1
Bart Frackiewicz <decomplosed@gmail.com>
2020-10-18 09:41:43 +0200

    spread restProps within Overlay

--- src/components/player/index.js
+++ src/components/player/index.js
@@ -19,7 +19,7 @@ Player.Video = function PlayerVideo({ src, ...restProps }) {
 
   return showPlayer
     ? ReactDOM.createPortal(
-        <Overlay onClick={() => setShowPlayer(false)}>
+        <Overlay onClick={() => setShowPlayer(false)} {...restProps}>
           <Inner>
             <video id='netflix-player' controls>
               <source src={src} type='video/mp4' />
