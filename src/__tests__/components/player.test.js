072f0d6f90660add1cffa76eab633c8c099af1a0
Bart Frackiewicz <decomplosed@gmail.com>
2020-10-19 21:32:07 +0200

    fixed Player component import

--- src/__tests__/components/player.test.js
+++ src/__tests__/components/player.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
-import { Player } from '../../componentsnts';
+import { Player } from '../../components';
 
 describe('<Player />', () => {
   it('Renders the <Player /> with a bunny video', () => {
