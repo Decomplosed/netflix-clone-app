065d99b41dbfda2e8d811a12348326208b70a4f8
Bart Frackiewicz <decomplosed@gmail.com>
2020-10-19 21:39:48 +0200

    fixed the Player template

--- src/__tests__/components/player.test.js
+++ src/__tests__/components/player.test.js
@@ -3,12 +3,11 @@ import { render, fireEvent } from '@testing-library/react';
 import { Player } from '../../components';
 
 describe('<Player />', () => {
-  it('Renders the <Player /> with a bunny video', () => {
+  it('renders the <Player /> with a bunny video', () => {
     const { container, getByText, queryByTestId } = render(
       <Player>
-        <Player.Button>
-          <Player.Video src='/videos/bunny.mp4' />
-        </Player.Button>
+        <Player.Button />
+        <Player.Video src='/videos/bunny.mp4' />
       </Player>,
     );
 
