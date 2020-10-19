23872e8c256f148aca41edad6f631837454395ee
Bart Frackiewicz <decomplosed@gmail.com>
2020-10-19 21:29:07 +0200

    created a test for Renders the <Player /> with a bunny video

--- src/__tests__/player.test.js
+++ src/__tests__/player.test.js
@@ -3,5 +3,5 @@ import { render, fireEvent } from '@testing-library/react';
 import { Player } from '../components';
 
 describe('<Player />', () => {
-  
-})
+  it('Renders the <Player /> with a bunny video', () => {});
+});
