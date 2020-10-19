d82c6e2336c14d00364c3ec8160ff874eb0abbbd
Bart Frackiewicz <decomplosed@gmail.com>
2020-10-19 21:31:19 +0200

    render Player component

--- src/__tests__/components/player.test.js
+++ src/__tests__/components/player.test.js
@@ -3,5 +3,13 @@ import { render, fireEvent } from '@testing-library/react';
 import { Player } from '../../componentsnts';
 
 describe('<Player />', () => {
-  it('Renders the <Player /> with a bunny video', () => {});
+  it('Renders the <Player /> with a bunny video', () => {
+    const { getByText, queryByTestId } = render(
+      <Player>
+        <Player.Button>
+          <Player.Video src='/videos/bunny.mp4' />
+        </Player.Button>
+      </Player>,
+    );
+  });
 });
