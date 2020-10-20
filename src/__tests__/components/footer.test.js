6a139025d19e417fa8cea48d4d86601806f0296c
Bart Frackiewicz <decomplosed@gmail.com>
2020-10-20 15:52:42 +0200

    added second column

--- src/__tests__/components/footer.test.js
+++ src/__tests__/components/footer.test.js
@@ -16,6 +16,13 @@ describe('<Footer />', () => {
             <Footer.Link href='#'>Corporate Information</Footer.Link>
             <Footer.Link href='#'>Netflix Originals</Footer.Link>
           </Footer.Column>
+
+          <Footer.Column>
+            <Footer.Link href='#'>Help Centre</Footer.Link>
+            <Footer.Link href='#'>Jobs</Footer.Link>
+            <Footer.Link href='#'>Terms of Use</Footer.Link>
+            <Footer.Link href='#'>Contact Us</Footer.Link>
+          </Footer.Column>
         </Footer.Row>
       </Footer>,
     );
