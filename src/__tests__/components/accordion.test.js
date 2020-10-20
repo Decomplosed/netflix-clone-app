9b9eaedbe203ad9c944513cf7f3011ed8e5dc13e
Bart Frackiewicz <decomplosed@gmail.com>
2020-10-20 16:01:26 +0200

    created main describe

--- src/__tests__/components/accordion.test.js
+++ src/__tests__/components/accordion.test.js
@@ -2,3 +2,5 @@ import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
 import faqsData from '../../fixtures/faqs';
 import { Accordion } from '../../components';
+
+describe('<Accordion />', () => {});
