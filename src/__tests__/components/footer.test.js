42511d7fe4c36628f2a51bacd0ea8d0699444aa1
Bart Frackiewicz <decomplosed@gmail.com>
2020-10-20 15:53:04 +0200

    added last column

--- src/__tests__/components/footer.test.js
+++ src/__tests__/components/footer.test.js
@@ -30,6 +30,13 @@ describe('<Footer />', () => {
             <Footer.Link href='#'>Privacy</Footer.Link>
             <Footer.Link href='#'>Speed Test</Footer.Link>
           </Footer.Column>
+
+          <Footer.Column>
+            <Footer.Link href='#'>Media Centre</Footer.Link>
+            <Footer.Link href='#'>Buy gift cards</Footer.Link>
+            <Footer.Link href='#'>Cookie Preferences</Footer.Link>
+            <Footer.Link href='#'>Legal Notices</Footer.Link>
+          </Footer.Column>
         </Footer.Row>
       </Footer>,
     );
