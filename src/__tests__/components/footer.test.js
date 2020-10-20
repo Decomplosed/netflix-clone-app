fa37e0d446043a11efebab2c212083ab53b30163
Bart Frackiewicz <decomplosed@gmail.com>
2020-10-20 15:51:03 +0200

    get container, getByText

--- src/__tests__/components/footer.test.js
+++ src/__tests__/components/footer.test.js
@@ -3,5 +3,7 @@ import { render } from '@testing-library/react';
 import { Footer } from '../../components';
 
 describe('<Footer />', () => {
-  it('Renders the <Footer /> with populated data', () => {});
+  it('Renders the <Footer /> with populated data', () => {
+    const { container, getByText } = render();
+  });
 });
