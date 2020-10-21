e21fe6bbc6444b6574743c1e197e00f1bff9217d
Bart Frackiewicz <decomplosed@gmail.com>
2020-10-21 08:12:13 +0200

    reender Accordion.Body

--- src/__tests__/components/accordion.test.js
+++ src/__tests__/components/accordion.test.js
@@ -12,6 +12,7 @@ describe('<Accordion />', () => {
           {faqsData.map((item) => (
             <Accordion.Item key={item.id}>
               <Accordion.Header>{item.header}</Accordion.Header>
+              <Accordion.Body>{item.body}</Accordion.Body>
             </Accordion.Item>
           ))}
         </Accordion.Frame>
