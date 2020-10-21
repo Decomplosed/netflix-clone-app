546ef2a19b00eec7896d92fd3e7c2973ffa70778
Bart Frackiewicz <decomplosed@gmail.com>
2020-10-21 08:30:01 +0200

    fire click event on 'What is Netflix?' element

--- src/__tests__/components/accordion.test.js
+++ src/__tests__/components/accordion.test.js
@@ -49,5 +49,6 @@ describe('<Accordion />', () => {
       "Netflix is a streaming service that offers a wide variety of award-winning TV programmes, films, anime, documentaries and more – on thousands of internet-connected devices. You can watch as much as you want, whenever you want, without a single advert – all for one low monthly price. There's always something new to discover, and new TV programmes and films are added every week!";
 
     expect(queryByText(whatIsNetflixBodyText)).toBeFalsy();
+    fireEvent.click(queryByText('What is Netflix?'));
   });
 });
