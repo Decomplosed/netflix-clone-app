49bdc87b83d21ad19a9b8f4c0625c5e18abd0a2e
Bart Frackiewicz <decomplosed@gmail.com>
2020-10-21 08:23:20 +0200

    moved second text inside main describe

--- src/__tests__/components/accordion.test.js
+++ src/__tests__/components/accordion.test.js
@@ -27,25 +27,25 @@ describe('<Accordion />', () => {
     expect(getByText('What can I watch on Netflix?')).toBeTruthy();
     expect(container.firstChild).toMatchSnapshot();
   });
-});
 
-it('Opens and closes the <Accordion /> component', () => {
-  const { container, queryByText } = render(
-    <Accordion>
-      <Accordion.Title>Frequently Asked Questions</Accordion.Title>
-      <Accordion.Frame>
-        {faqsData.map((item) => (
-          <Accordion.Item key={item.id}>
-            <Accordion.Header>{item.header}</Accordion.Header>
-            <Accordion.Body data-testid='accordion-body'>
-              {item.body}
-            </Accordion.Body>
-          </Accordion.Item>
-        ))}
-      </Accordion.Frame>
-    </Accordion>,
-  );
+  it('Opens and closes the <Accordion /> component', () => {
+    const { container, queryByText } = render(
+      <Accordion>
+        <Accordion.Title>Frequently Asked Questions</Accordion.Title>
+        <Accordion.Frame>
+          {faqsData.map((item) => (
+            <Accordion.Item key={item.id}>
+              <Accordion.Header>{item.header}</Accordion.Header>
+              <Accordion.Body data-testid='accordion-body'>
+                {item.body}
+              </Accordion.Body>
+            </Accordion.Item>
+          ))}
+        </Accordion.Frame>
+      </Accordion>,
+    );
 
-  const whatIsNetflixBodyText =
-    "Netflix is a streaming service that offers a wide variety of award-winning TV programmes, films, anime, documentaries and more – on thousands of internet-connected devices. You can watch as much as you want, whenever you want, without a single advert – all for one low monthly price. There's always something new to discover, and new TV programmes and films are added every week!";
+    const whatIsNetflixBodyText =
+      "Netflix is a streaming service that offers a wide variety of award-winning TV programmes, films, anime, documentaries and more – on thousands of internet-connected devices. You can watch as much as you want, whenever you want, without a single advert – all for one low monthly price. There's always something new to discover, and new TV programmes and films are added every week!";
+  });
 });
