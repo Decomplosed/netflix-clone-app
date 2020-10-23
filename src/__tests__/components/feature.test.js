ee32cc2bd41d11444d2032dc64f86ea8f55eee9c
Bart Frackiewicz <decomplosed@gmail.com>
2020-10-23 08:57:35 +0200

    get container, getByText

--- src/__tests__/components/feature.test.js
+++ src/__tests__/components/feature.test.js
@@ -3,5 +3,7 @@ import { render } from '@testing-library/react';
 import { Feature } from '../../components';
 
 describe('<Feature />', () => {
-  it('Renders the <Feature /> with populated data', () => {});
+  it('Renders the <Feature /> with populated data', () => {
+    const { container, getByText } = render();
+  });
 });
