4976bb49db195e2b382ba0cf98e37f74a39edbc3
Bart Frackiewicz <decomplosed@gmail.com>
2020-10-25 08:20:59 +0100

    expect loading element to be true

--- src/__tests__/components/loading.test.js
+++ src/__tests__/components/loading.test.js
@@ -4,6 +4,10 @@ import { Loading } from '../../components';
 
 describe('<Loading />', () => {
   it('Renders the <Loading />', () => {
-    const { container, getByTestId } = render(<Loading src="/images/karl.png" data-testid="loading" />);
+    const { container, getByTestId } = render(
+      <Loading src='/images/karl.png' data-testid='loading' />,
+    );
+
+    expect(getByTestId('loading')).toBeTruthy();
   });
 });
