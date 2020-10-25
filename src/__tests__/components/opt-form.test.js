d51a24077cb1f6fef1bd8903c5b6bde23559cb45
Bart Frackiewicz <decomplosed@gmail.com>
2020-10-25 08:15:08 +0100

    new test for 'Renders the <OptForm /> with populated data'

--- src/__tests__/components/opt-form.test.js
+++ src/__tests__/components/opt-form.test.js
@@ -2,4 +2,6 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import { OptForm } from '../../components';
 
-describe('<OptForm />', () => {});
+describe('<OptForm />', () => {
+  it('Renders the <OptForm /> with populated data', () => {});
+});
