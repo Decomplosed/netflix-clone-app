858a24c05cbff5e16fdb0630cfcef4c0932a2f54
Bart Frackiewicz <decomplosed@gmail.com>
2020-10-25 08:15:28 +0100

    get container, getByText, getByPlaceholderText

--- src/__tests__/components/opt-form.test.js
+++ src/__tests__/components/opt-form.test.js
@@ -3,5 +3,7 @@ import { render } from '@testing-library/react';
 import { OptForm } from '../../components';
 
 describe('<OptForm />', () => {
-  it('Renders the <OptForm /> with populated data', () => {});
+  it('Renders the <OptForm /> with populated data', () => {
+    const { container, getByText, getByPlaceholderText } = render();
+  });
 });
