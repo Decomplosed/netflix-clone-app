c00635da842cd2c1e0e8f14509bcbd5001f07168
Bart Frackiewicz <decomplosed@gmail.com>
2020-10-25 08:24:17 +0100

    expect container to match snapshot

--- src/__tests__/components/loading.test.js
+++ src/__tests__/components/loading.test.js
@@ -10,5 +10,6 @@ describe('<Loading />', () => {
 
     expect(getByTestId('loading')).toBeTruthy();
     expect(getByTestId('loading-picture')).toBeTruthy();
+    expect(container.firstChild).toMatchSnapshot();
   });
 });
