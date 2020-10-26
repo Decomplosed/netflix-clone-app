16ed3f95c502efb4dbb97db46ad6911b62813626
Bart Frackiewicz <decomplosed@gmail.com>
2020-10-26 23:23:07 +0100

    new test for 'Renders the <Profiles /> with populated data'

--- src/__tests__/components/profiles.test.js
+++ src/__tests__/components/profiles.test.js
@@ -2,4 +2,6 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import { Profiles } from '../../components';
 
-describe('<Profiles />', () => {});
+describe('<Profiles />', () => {
+  it('Renders the <Profiles /> with populated data', () => {});
+});
