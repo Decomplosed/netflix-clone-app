4abcd256cc90ecf70ec11214dda7d99af825b39f
Bart Frackiewicz <decomplosed@gmail.com>
2020-10-27 07:54:47 +0100

    test for 'Renders the basic <Header /> with a background'

--- src/__tests__/components/header.test.js
+++ src/__tests__/components/header.test.js
@@ -4,4 +4,6 @@ import { Header } from '../../components';
 
 jest.mock('react-router-dom');
 
-describe('<Header />', () => {});
+describe('<Header />', () => {
+  it('Renders the basic <Header /> with a background', () => {});
+});
