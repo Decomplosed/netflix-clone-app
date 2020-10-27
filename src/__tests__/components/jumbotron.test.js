6054cd9e364f237f05c987a3083d2332f911c2ff
Bart Frackiewicz <decomplosed@gmail.com>
2020-10-27 07:45:53 +0100

    render Jumbotron.Image

--- src/__tests__/components/jumbotron.test.js
+++ src/__tests__/components/jumbotron.test.js
@@ -13,7 +13,13 @@ describe('<Jumbotron />', () => {
               <Jumbotron.Title>{item.title}</Jumbotron.Title>
               <Jumbotron.SubTitle>{item.subTitle}</Jumbotron.SubTitle>
             </Jumbotron.Pane>
-            <Jumbotron.Pane></Jumbotron.Pane>
+            <Jumbotron.Pane>
+              <Jumbotron.Image
+                src={item.image}
+                alt={item.alt}
+                data-testid={`${item.id}-jumbo-image`}
+              />
+            </Jumbotron.Pane>
           </Jumbotron>
         ))}
       </Jumbotron.Container>,
