56292ae03e3834fc10c461fba853f8a0940e7abe
Bart Frackiewicz <decomplosed@gmail.com>
2020-10-27 07:55:03 +0100

    get container, getByText, getByTestId

--- src/__tests__/components/header.test.js
+++ src/__tests__/components/header.test.js
@@ -5,5 +5,7 @@ import { Header } from '../../components';
 jest.mock('react-router-dom');
 
 describe('<Header />', () => {
-  it('Renders the basic <Header /> with a background', () => {});
+  it('Renders the basic <Header /> with a background', () => {
+    const { container, getByText, getByTestId } = render();
+  });
 });
