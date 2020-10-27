aad13013cb56193d79280c6851294f40b938ba33
Bart Frackiewicz <decomplosed@gmail.com>
2020-10-27 07:55:29 +0100

    render Header.Frame

--- src/__tests__/components/header.test.js
+++ src/__tests__/components/header.test.js
@@ -7,7 +7,9 @@ jest.mock('react-router-dom');
 describe('<Header />', () => {
   it('Renders the basic <Header /> with a background', () => {
     const { container, getByText, getByTestId } = render(
-      <Header></Header>
+      <Header>
+        <Header.Frame></Header.Frame>
+      </Header>,
     );
   });
 });
