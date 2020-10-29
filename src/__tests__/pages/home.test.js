e7d934f27cd7c2b48b3151d94ebb92d55b43505f
Bart Frackiewicz <decomplosed@gmail.com>
2020-10-29 08:16:24 +0100

    expect element with matching tet to be true

--- src/__tests__/pages/home.test.js
+++ src/__tests__/pages/home.test.js
@@ -11,4 +11,9 @@ test('Renders the homepage', () => {
   expect(getByText('Watch anywhere. Cancel at any time.')).toBeTruthy();
   expect(getAllByPlaceholderText('Email address')).toBeTruthy();
   expect(getAllByText('Try it now')).toBeTruthy();
+  expect(
+    getAllByText(
+      'Ready to watch? Enter your email to create or restart your membership.',
+    ),
+  ).toBeTruthy();
 });
