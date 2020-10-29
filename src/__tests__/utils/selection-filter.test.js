3e40dd69993e7de93a4c7c73377c7cab7c2edeba
Bart Frackiewicz <decomplosed@gmail.com>
2020-10-29 08:39:05 +0100

    created series array

--- src/__tests__/utils/selection-filter.test.js
+++ src/__tests__/utils/selection-filter.test.js
@@ -1,3 +1,19 @@
 import { selectionFilter } from '../../utils';
 
 test('selectionFilter with legitimate data', () => {});
+const series = [
+  {
+    title: 'Documentaries',
+    data: [
+      {
+        id: 'series-1x',
+        title: 'Tiger King',
+        description:
+          'An exploration of big cat breeding and its bizarre underworld, populated by eccentric characters.',
+        genre: 'documentaries',
+        maturity: '18',
+        slug: 'tiger-king',
+      },
+    ],
+  },
+];
