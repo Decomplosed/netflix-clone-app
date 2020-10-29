f7e44e6bcf355069ce7726df7eeac8531e1e5c6b
Bart Frackiewicz <decomplosed@gmail.com>
2020-10-29 08:16:07 +0100

    expect 'Try it now' button to be true

--- src/__tests__/pages/home.test.js
+++ src/__tests__/pages/home.test.js
@@ -10,4 +10,5 @@ test('Renders the homepage', () => {
   expect(getByText('Unlimited films, TV programmes and more.')).toBeTruthy();
   expect(getByText('Watch anywhere. Cancel at any time.')).toBeTruthy();
   expect(getAllByPlaceholderText('Email address')).toBeTruthy();
+  expect(getAllByText('Try it now')).toBeTruthy();
 });
