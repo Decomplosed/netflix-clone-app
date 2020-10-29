d1c7b26f52b81f661528ee7b2bf5c520ee0f2279
Bart Frackiewicz <decomplosed@gmail.com>
2020-10-29 08:39:55 +0100

    moved series and films array inside the test

--- src/__tests__/utils/selection-filter.test.js
+++ src/__tests__/utils/selection-filter.test.js
@@ -1,29 +1,30 @@
 import { selectionFilter } from '../../utils';
 
-test('selectionFilter with legitimate data', () => {});
-const series = [
-  {
-    title: 'Documentaries',
-    data: [
-      {
-        id: 'series-1x',
-        title: 'Tiger King',
-        description:
-          'An exploration of big cat breeding and its bizarre underworld, populated by eccentric characters.',
-        genre: 'documentaries',
-        maturity: '18',
-        slug: 'tiger-king',
-      },
-    ],
-  },
-];
-const films = [
-  {
-    id: 'film-1x',
-    title: 'The Prestige',
-    description: 'Great film...',
-    genre: 'drama',
-    maturity: '15',
-    slug: 'the-prestige',
-  },
-];
+test('selectionFilter with legitimate data', () => {
+  const series = [
+    {
+      title: 'Documentaries',
+      data: [
+        {
+          id: 'series-1x',
+          title: 'Tiger King',
+          description:
+            'An exploration of big cat breeding and its bizarre underworld, populated by eccentric characters.',
+          genre: 'documentaries',
+          maturity: '18',
+          slug: 'tiger-king',
+        },
+      ],
+    },
+  ];
+  const films = [
+    {
+      id: 'film-1x',
+      title: 'The Prestige',
+      description: 'Great film...',
+      genre: 'drama',
+      maturity: '15',
+      slug: 'the-prestige',
+    },
+  ];
+});
