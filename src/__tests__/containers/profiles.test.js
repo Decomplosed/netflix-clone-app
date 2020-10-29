c70028f37fd26dc417c6f4f6070225b4260ddfd6
Bart Frackiewicz <decomplosed@gmail.com>
2020-10-29 08:22:08 +0100

    render SelectProfileContainer

--- src/__tests__/containers/profiles.test.js
+++ src/__tests__/containers/profiles.test.js
@@ -8,6 +8,6 @@ describe('<Profiles />', () => {
   it('Renders the <Profiles />', () => {
     const user = { displayName: 'Bart', photoURL: 'profile.png' };
     const setProfile = jest.fn();
-    const { getByTestId } = render();
+    const { getByTestId } = render(<SelectProfileContainer />);
   });
 });
