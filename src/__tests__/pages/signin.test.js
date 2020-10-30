d9422b17af76afc23edc771101cce653166ff061
Bart Frackiewicz <decomplosed@gmail.com>
2020-10-30 15:21:33 +0100

    mock react-router-dom

--- src/__tests__/pages/signin.test.js
+++ src/__tests__/pages/signin.test.js
@@ -4,3 +4,5 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { act } from 'react-dom/test-utils';
 import { SignIn } from '../../pages';
 import { FirebaseContext } from '../../context/firebase';
+
+jest.mock('react-router-dom', () => ({}));
