be8781b5f50b4a04a093fbbbff9337542a9430e2
Bart Frackiewicz <decomplosed@gmail.com>
2020-10-30 15:14:28 +0100

    expect data to be undefined

--- src/__tests__/utils/selection-filter.test.js
+++ src/__tests__/utils/selection-filter.test.js
@@ -39,4 +39,5 @@ test('selectionFilter with no data', () => {
   const slides = selectionFilter();
   expect(slides.series[0].title).toBe('Documentaries');
   expect(slides.films[0].title).toBe('Drama');
+  expect(slides.series[0].data).toBe(undefined);
 });
