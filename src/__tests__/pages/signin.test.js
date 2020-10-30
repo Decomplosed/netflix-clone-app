bd37dccbe13cc0f1ef713ce30cdfd58358956e89
Bart Frackiewicz <decomplosed@gmail.com>
2020-10-30 15:21:14 +0100

    imported in FirebaseContext

--- src/__tests__/pages/signin.test.js
+++ src/__tests__/pages/signin.test.js
@@ -3,3 +3,4 @@ import { render, fireEvent } from '@testing-library/react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { act } from 'react-dom/test-utils';
 import { SignIn } from '../../pages';
+import { FirebaseContext } from '../../context/firebase';
