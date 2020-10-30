02f87569de47d40aeadbd635fc1aa13682c0c9b6
Bart Frackiewicz <decomplosed@gmail.com>
2020-10-30 15:21:01 +0100

    imported in SignIn

--- src/__tests__/pages/signin.test.js
+++ src/__tests__/pages/signin.test.js
@@ -2,3 +2,4 @@ import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { act } from 'react-dom/test-utils';
+import { SignIn } from '../../pages';
