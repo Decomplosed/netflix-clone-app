874aa5a16cec01a2509ae9f0c84ac542d698ee2d
Bart Frackiewicz <decomplosed@gmail.com>
2020-11-01 08:05:40 +0100

    turned test to be async

--- src/__tests__/pages/browse.test.js
+++ src/__tests__/pages/browse.test.js
@@ -64,7 +64,7 @@ const firebase = {
 };
 
 describe('<Browse />', () => {
-  it('Renders the browse page with <SelectProfileContainer />', () => {
+  it('Renders the browse page with <SelectProfileContainer />', async () => {
     const { getByTestId, getByPlaceholderText, queryByTestId } = render();
   });
 });
